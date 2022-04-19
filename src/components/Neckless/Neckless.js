135a9ea1eb153fcb75afcd0d9dc03a28bba5530d
Rakshith G <rakshithgowdag89@gmail.com>
2022-04-19 11:44:37 +0530

    init

--- src/components/Neckless/Neckless.js
+++ src/components/Neckless/Neckless.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import './Neckless.css'
+
+
 const Neckless = () => {
-    return(<div className='container'>
-        
+
+
+    function parallax(event) {
+        document.querySelectorAll(".image").forEach((shift) => {
+          let position = shift.getAttribute("value");
+          let x = (window.innerWidth - event.pageX * position) / 90;
+          let y = (window.innerHeight - event.pageY * position) / 90;
+            console.log(x,y)
+          shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        });
+      }
+
+
+    return(
+    <div className='container' >
+        <div className = "image-wrapper" onMouseMove={parallax}>
+            <div className='image'></div>
+        </div>
+        <h2 className='title'>Neckless</h2>
     </div>)
 }
 
